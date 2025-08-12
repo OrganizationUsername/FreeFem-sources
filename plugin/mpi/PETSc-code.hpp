OrganizationUsername/FreeFem-sources

commit 650b6c3ca32b2b565627c5088dea8ac7c4b1eb73

Fix compilation --with-precision=single
Reported-by: @tomcaruso

plugin/mpi/PETSc-code.hpp
@@ -3847,6 +3847,7 @@ namespace PETSc {
         nargs[2] && c != 1 ? GetAny< Matrice_Creuse< double >* >((*nargs[2])(stack)) : nullptr;
       PetscInt bs;
       MatGetBlockSize(ptB->_petsc, &bs);
+      KN< double >* empty = nullptr;
       if (!pList) {
         ptA->_A = new HpddmType(static_cast< const HPDDM::Subdomain< PetscScalar >& >(*ptB->_A));
         ptA->_A->setMatrix(dA);
@@ -3858,14 +3859,14 @@ namespace PETSc {
             ptA->_D = new KN<PetscReal>(dA->HPDDM_n);
             for (int i = 0; i < dA->HPDDM_n; ++i) ptA->_D->operator[](i) = ptB->_A->getScaling()[i];
         }
+        empty = new KN< double >(dA->HPDDM_n, (double*)(ptA->_D));
         initPETScStructure<D>(ptA, bs,
           nargs[1] && GetAny< bool >((*nargs[1])(stack)) ? PETSC_TRUE : PETSC_FALSE,
-          ptA->_D);
+          empty);
       } else {
         int n = ptB->_A->getDof();
         ffassert(dA->HPDDM_n == n);
         HPDDM::MatrixCSR< void >* L;
-        KN< double >* empty = nullptr;
         if (pList->A) {
           MatriceMorse< double >* mList = static_cast< MatriceMorse< double >* >(&*(pList->A));
           mList->CSR( );
@@ -3896,10 +3897,10 @@ namespace PETSc {
         ptA->_num = new PetscInt[ptA->_A->getDof()];
         initPETScStructure<D>(ptA, bs,
           nargs[1] && GetAny< bool >((*nargs[1])(stack)) ? PETSC_TRUE : PETSC_FALSE, empty);
-        delete empty;
         if (c != 0 || !ptK->A)
             delete dA;
       }
+      delete empty;
       if (c == 1) {
         MatSetType(ptA->_petsc, MATSHELL);
         User< LinearSolver< Dmat > > user = nullptr;

commit 81b32dc38cb410dceea9d90aded0a00011d5b649

Fix memory error introduced in 650b6c3ca32b2b565627c5088dea8ac7c4b1eb73
Reported-by: @cmd8

plugin/mpi/PETSc-code.hpp
@@ -3858,11 +3858,19 @@ namespace PETSc {
         if (ptB->_A->getScaling()) {
             ptA->_D = new KN<PetscReal>(dA->HPDDM_n);
             for (int i = 0; i < dA->HPDDM_n; ++i) ptA->_D->operator[](i) = ptB->_A->getScaling()[i];
+#if !defined(PETSC_USE_REAL_DOUBLE)
+            empty = new KN<double>(dA->HPDDM_n);
+            for (int i = 0; i < dA->HPDDM_n; ++i) empty->operator[](i) = ptB->_A->getScaling()[i];
+#else
+            empty = ptA->_D;
+#endif
         }
-        empty = new KN< double >(dA->HPDDM_n, (double*)(ptA->_D));
         initPETScStructure<D>(ptA, bs,
           nargs[1] && GetAny< bool >((*nargs[1])(stack)) ? PETSC_TRUE : PETSC_FALSE,
           empty);
+#if defined(PETSC_USE_REAL_DOUBLE)
+        empty = nullptr;
+#endif
       } else {
         int n = ptB->_A->getDof();
         ffassert(dA->HPDDM_n == n);