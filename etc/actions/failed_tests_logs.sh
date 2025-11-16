83b244de95673b3c1211f5a93d9c81f4e360fe20
Simon Legrand <simon.legrand@inria.fr>
2025-11-16 17:51:44 +0100

    [CI] Tests failed properly and logs are printed

--- etc/actions/failed_tests_logs.sh
+++ etc/actions/failed_tests_logs.sh
@@ -1,12 +1,24 @@
 #!/usr/bin/env bash
 
-FAILED_TESTS=( $(grep :test-result examples/*/*.trs | grep FAIL) )
+# This script checks if tests have failed. If yes, it prints the content of the
+# failed tests log, and returns 1.
+#
+# It allows keeping the '-i' option for 'make check', print the failed
+# tests, and only then, return exit 1 to interrupt the CI pipeline.
 
-for val in ${FAILED_TESTS[@]/#*FAIL/}
-do
-    IFS="::"
-    read -ra path <<< $val
+FAILED_TESTS=$(grep ":test-result: FAIL" examples/*/*.trs)
+
+if [ -z "$FAILED_TESTS" ]; then
+    exit 0;
+fi
+
+NB_FAILED_TESTS=$(echo "$FAILED_TESTS" | wc -l)
+echo "$NB_FAILED_TESTS tests failed:"
+
+echo "$FAILED_TESTS" | while IFS= read -r line; do
+    path="${line%::test-result: FAIL}"
     printf  "\n******** ${path%.*}.log ********\n\n"
     tail -n 100 ${path%.*}.log
-    unset IFS
-done;
+done
+
+exit 1
